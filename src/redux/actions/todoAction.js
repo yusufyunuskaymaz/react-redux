67307f464ab8a34e728eab90233064f467b01ac3
yusufKaymaz <kaymazyusuf88@gmail.com>
2022-11-16 10:23:44 +0300

    first commit

---
+++ src/redux/actions/todoAction.js
@@ -0,0 +1,13 @@
+import { ADD_TODO, CLEAR_TODO } from "../types/todoTypes";
+
+export const addTodo = (payload) => {
+  return {
+    type: ADD_TODO,
+    payload: payload,
+  };
+};
+export const clearTodo = () => {
+  return {
+    type: CLEAR_TODO,
+  };
+};
---
+++ src/redux/reducers/todoReducer.js
@@ -0,0 +1,27 @@
+import { ADD_TODO, CLEAR_TODO, DELETE_TODO, TOGGLE_TODO } from "../types/todoTypes"
+
+const initialState = {
+    todoList:[{
+      id: new Date().getTime(),
+      text:"Work Hard",
+      completed:true
+    }]
+}
+
+export const todoReducer =  (state = initialState, { type, payload }) => {
+  switch (type) {
+
+  case ADD_TODO:
+    return {
+      todoList: [...state.todoList, {id:new Date().getTime(), text:payload, completed:false}]
+    }
+  case DELETE_TODO:
+    return {}
+  case TOGGLE_TODO:
+    return {};
+    case CLEAR_TODO:
+    return initialState
+  default:
+    return state
+  }
+}
